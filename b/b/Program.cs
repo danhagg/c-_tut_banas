b2ef45c992c8a8fe42301a10e9a34e453082b441
danhagg <danielhaggerty1976@gmail.com>
2018-05-15 16:29:24 -0500

    intro

---
+++ b/b/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CSharpTut1.cs
+{
+    class Program
+    {
+
+        // main function, code starts executing here
+        // static, this function belongs to class and can be executed without object
+        // void, doesnt return a value after executing (may be int 0 or -1)
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello");
+
+            for (int i = 1; i < 5; i++)
+            {
+                Console.WriteLine("Arg {0}", i);
+            }
+
+            SayHello();
+
+            Console.ReadLine();
+        }
+
+        private static void SayHello()
+        {
+            string name = "";
+            Console.Write("What is your name");
+            name = Console.ReadLine();
+            Console.WriteLine("Hello {0}: ", name);
+        }
+
+    }
+}
